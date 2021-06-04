83aabd46bdb4abb0bf391b20474bb970b64f2f62
BaekJungAh <rurounida88@gmail.com>
2021-06-04 23:04:11 +0900

    Create Multiplication Table file

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성하세요.
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.print(i);
+                System.out.print(" * ");
+                System.out.print(j);
+                System.out.print(" = ");
+                System.out.println(i*j);
+            }
+        }
+    }
+}
